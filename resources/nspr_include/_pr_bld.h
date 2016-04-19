64767b83046c94dfc1a1a55bd7824cb414a942cb
akashche <akashche@redhat.com>
2016-04-19 15:27:55 -0700

    license headers added

--- resources/nspr_include/_pr_bld.h
+++ resources/nspr_include/_pr_bld.h
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #define _BUILD_STRING "2016-02-09 19:24:02"
 #define _BUILD_TIME 1455045842000000LL
 #define _PRODUCTION "libnspr4.so"
